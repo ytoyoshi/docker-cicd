f86412f0e41d9fe6e22eab54381a7eec6d501eac
ytoyoshi <113228101+ytoyoshi@users.noreply.github.com>
2024-10-19 13:13:17 +0900

    20241019

--- dockercicdapp/views/views.py
+++ dockercicdapp/views/views.py
@@ -35,6 +35,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            return redirect('home')
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
